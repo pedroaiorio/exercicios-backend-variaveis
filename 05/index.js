431b8cf014035a40b052130e949f6543a1edaad6
pedroaiorio <pedroaiorio@hotmail.com>
2023-07-17 21:20:47 -0300

    Exercicio 5 resolvido

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,11 @@
+function distanciaEntrePontos(x1, y1, x2, y2) {
 
+    const p1 = (x2 - x1) ** 2 + (y2 - y1) ** 2
+    const d = Math.sqrt(p1, 2)
+
+    return d
+
+}
+
+
+console.log(distanciaEntrePontos(1, 1, 1, 4))
