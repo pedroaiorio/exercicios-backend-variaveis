f07fcf4a244d7d175c6bc5d523d0515bdeb12133
pedroaiorio <pedroaiorio@hotmail.com>
2023-07-17 21:47:02 -0300

    Exercicio 8 resolvido

---
+++ 08/index.js
@@ -0,0 +1,14 @@
+/*c = 2piR
+A = piR2*/
+
+
+function informacoesCirculo(r) {
+
+    const c = 2 * Math.PI * r
+    const a = Math.PI * r ** 2
+
+    console.log(`O comprimento do circulo é igual ${c} e a área do circulo é igual a ${a}`)
+
+}
+
+informacoesCirculo(2)
