7c800e2edd8239ebb7309436ce0181694cce8f45
pedroaiorio <pedroaiorio@hotmail.com>
2023-07-17 21:55:29 -0300

    Exercicio 9 resolvido

---
+++ 09/index.js
@@ -0,0 +1,9 @@
+function somaAngulos(n) {
+    const s = (n - 2) * 180
+    const a = s / n
+
+    console.log(`A soma interna do ângulos é igual ${s} e cada ângulo é igual a ${a}`)
+}
+
+
+somaAngulos(6)
